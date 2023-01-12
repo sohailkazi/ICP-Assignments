408242d65d3f871c3429c8a76e053f9c3e81bb82
Sohail <sohailkazi001@gmail.com>
2023-01-12 21:40:29 +0530

    added references

--- Assignment 5/3_reverse_string.c
+++ Assignment 5/3_reverse_string.c
@@ -1,39 +1,61 @@
 #include <stdio.h>
-
-void reverse(char* begin, char* end)
+void reverse(char arr[], int n)
 {
-    char temp;
-    while (begin < end) {
-        temp = *begin;
-        *begin++ = *end;
-        *end-- = temp;
+    //reverses word
+    for(int i = 0; i < n/2; i++)
+    {
+        char temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
     }
 }
  
-void reverseWords(char* s)
-{
-    char* word_begin = s;
-    char* temp = s;
- 
-    while (*temp) {
-        temp++;
-        if (*temp == '\0') {
-            reverse(word_begin, temp - 1);
-        }
-        else if (*temp == ' ') {
-            reverse(word_begin, temp - 1);
-            word_begin = temp + 1;
-        }
-    }
-    reverse(s, temp - 1);
-}
-
 int main()
 {
-    char s[] = "I Hate Cornflakes";
-    char* temp = s;
- 
-    reverseWords(s);
-    printf("%s", s);
-    return 0;
+    char str[100] = "I love you";
+    
+    int size = 0;
+    char buff[100];
+    char result[100];
+    
+    for(size = 0; str[size] != '\0'; size++);
+    
+    //maintains result array;
+    int j = 0;
+    //maintains buffer array;
+    int k = 0;
+    
+    for(int i = size; i >= 0; i--, k++)
+    {
+        if(str[i] == ' ')
+        {
+            buff[k] = '\0';
+            reverse(buff, k);
+            for(int s = 0; s < k; s++)
+            {
+                result[j] = buff[s];
+                j++;
+            }
+            k = 0;
+            result[j - 1] = ' ';
+            
+        }
+        else if(i == 0)
+        {
+            buff[k] = str[i];
+            k++;
+            buff[k] = '\0';
+            reverse(buff, k);
+            for(int s = 0; s < k - 1 ; s++)
+            {
+                result[j] = buff[s];
+                j++;
+            }
+        }
+        else
+        {
+            buff[k] = str[i];
+        }
+    }
+    printf("%s", result);
 }
